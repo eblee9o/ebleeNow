b8b0d869e0a9f09a7d60127c31203da2fd23d3bf
alexxx9o <hello.eblee@gmail.com>
2022-02-14 23:15:17 +0900

    commit

---
+++ ebleeNow/src/javaStudy/OopClass002.java
@@ -0,0 +1,39 @@
+package javaStudy;
+
+//class Person() {
+//	
+//	//1. 加己
+//	int age;
+//	String name;
+//	
+//	//2. 积己磊
+//	Person(){}
+//	Person(int a, String b){
+//		this.age = a;
+//		this.name = b;
+//	}
+//	
+//	//3. 皋辑靛
+//	void printPerson() {
+//		System.out.println("age >>> " + age);
+//		System.out.println("name >>> " + name);
+//	}
+//	
+//}//Person
+
+public class OopClass002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//按眉 积己
+//		Person ps = new Person(20, "abc");
+//		System.out.println(ps);
+		
+//		System.out.println(ps.age);
+//		System.out.println(ps.name);
+
+		
+	}
+
+}
---
+++ ebleeNow/src/javaStudy/OopParseInt002.java
@@ -0,0 +1,17 @@
+package javaStudy;
+
+public class OopParseInt002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("2022");
+		System.out.println(Integer.parseInt("2022",10));
+		System.out.println(Integer.parseInt("1",2));
+		System.out.println(Integer.parseInt("1001",2));
+		System.out.println(Integer.parseInt("1004",8));
+		System.out.println(Integer.parseInt("A",16));
+
+		
+	}//main
+}//class
