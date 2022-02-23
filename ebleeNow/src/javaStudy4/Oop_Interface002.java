8d4a808f5f80143bca069f0984ec646cecb5d206
alexxx9o <hello.eblee@gmail.com>
2022-02-23 23:22:25 +0900

    23/02/2022

---
+++ ebleeNow/src/javaStudy4/Oop_Interface002.java
@@ -0,0 +1,13 @@
+package javaStudy4;
+
+public class Oop_Interface002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println(">>>>");
+	}//main
+
+}//class
